f9cf92ae74ec320bfdeff63771ed436adce5d5c5
dpastor <David.Pastor.Herranz@telefonica.com>
2024-12-20 12:11:32 +0100

    ANDROID-15570 Configure release publication (#4)
    * ANDROID-15570 Configure release publication
    
    * ANDROID-15570 Fix other publication details
    
    * ANDROID-15570 Incorrect plugin reference
    
    * ANDROID-15570 Final GAs configuration
    
    * ANDROID-15570 Configure apache license file
    
    * ANDROID-15570 Add attributions
    
    * ANDROID-15570 Change runners before making repository public
    
    * ANDROID-15570 Typo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin)
-    id("com.telefonica.loggerazzi")
+    id("com.telefonica.loggerazzi-plugin")
 }
 
 android {
--- include-build/gradle-plugin/build.gradle.kts
+++ include-build/gradle-plugin/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     id("java-gradle-plugin")
+    alias(libs.plugins.publish.plugin)
 }
 
 java {
@@ -19,16 +20,14 @@ dependencies {
 
 gradlePlugin {
     plugins {
-        create("loggerazzi") {
-            id = "com.telefonica.loggerazzi"
+        create("loggerazzi-plugin") {
+            id = "com.telefonica.loggerazzi-plugin"
+            displayName = "Loggerazzi"
+            description = "Logs snapshot testing for Android Instrumentation tests"
             implementationClass = "com.telefonica.loggerazzi.LoggerazziPlugin"
+            website = "https://github.com/Telefonica/android-loggerazzi"
+            vcsUrl = "https://github.com/Telefonica/android-loggerazzi"
+            tags = listOf("android", "instrumentation", "testing", "logs")
         }
     }
 }
-
-allprojects {
-    group = "com.telefonica.loggerazzi"
-    version = System.getProperty("LIBRARY_VERSION") ?: "undefined"
-}
-
-apply("${rootProject.projectDir}/mavencentral.gradle")
